993bff1e536f59f8cfefbb3973eb3cb5213f509c
basephyson <152825219+basephyson@users.noreply.github.com>
2023-12-11 11:05:24 +0900

    Add files via upload

--- tt.py
+++ tt.py
@@ -77,18 +77,23 @@
 
 import matplotlib.pyplot as plt
 import streamlit as st
+# import numpy as np 
+
 
 fig, ax = plt.subplots()
 
-c1= st.sidebar.radio('선의 색을 선택하시오',['red','green','blue'])
-s1= st.sidebar.radio('선의 스타일을 선택하시오.',['-','--'])
-m1= st.sidebar.radio('점의 스타일을 선택하시오.',['o','s'])
+c1 = st.sidebar.radio('선의 색을 선택하시오',['red','green','blue','orange'])
+s1 = st.sidebar.radio('선의 스타일을 선택하시오.',['-','--',':','-.'])
+m1 = st.sidebar.radio('점의 스타일을 선택하시오.',['o','s','h','p'])
+
 
 holy= []
 y = []
 for i in range(-20,21,3):
-    holy.append(i)
-    y.append(-2*i*1 + 3*i + 5)
 
-plt.plot(holy, y, color = 'c1', linestyle = 's1', marker = 'm1' )
+    holy.append(i)
+    y.append(-2*i*i + 3*i + 5)
+
+plt.plot(holy, y, color = c1, linestyle = s1, marker = m1 )
 st.pyplot(fig)
+
